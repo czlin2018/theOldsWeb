03828766fd93bae70e658b6b05217a6ee37a6956
czlin <953564227@qq.com>
2019-03-04 01:41:12 +0800

    2019.3.4

--- src/main/java/com/theoldsweb/myweb/web/controller/tourController.java
+++ src/main/java/com/theoldsweb/myweb/web/controller/tourController.java
@@ -1,22 +1,20 @@
 package com.theoldsweb.myweb.web.controller;
 
-import com.theoldsweb.myweb.common.api.QiniuUtil;
-import com.theoldsweb.myweb.common.api.DateApi;
 import com.theoldsweb.myweb.common.config.PageDto;
 import com.theoldsweb.myweb.common.config.ResultDto;
-import com.theoldsweb.myweb.common.config.SysExcCode;
 import com.theoldsweb.myweb.common.dto.TourtbDto;
 import com.theoldsweb.myweb.common.url.Url;
 import com.theoldsweb.myweb.web.service.TourService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.util.Map;
 
 /**
- * @描述:
+ * @描述: 旅游tab页控制器
  * @版权: Copyright (c) 2016-2018
  * @公司: lumi
  * @作者: 泽林
@@ -24,43 +22,34 @@ import java.io.IOException;
  * @创建时间: 下午12:27r
  */
 @RestController
-public class tourController {
+public class TourController {
 
     @Autowired
     private TourService toutService;
-    @Autowired
-    private QiniuUtil qiniuUtil;
 
+    /**
+     * 旅游列表
+     */
     @GetMapping(value=Url.tour.getTourList)
     public ResultDto getTourAll ( PageDto pageDto ){
         return toutService.getTourAll(pageDto);
     }
 
+    /**
+     * 旅游添加
+     */
     @PostMapping(value=Url.tour.insert)
     public ResultDto insert ( @RequestBody TourtbDto tourtbDto ){
         return toutService.insert(tourtbDto);
     }
 
-    @PostMapping(value=Url.tour.selectByOne)
-    public ResultDto selectByOne ( @RequestBody TourtbDto tourtbDto ){
-        return toutService.selectByOne(tourtbDto);
+    /**
+     * 评论
+     */
+    @PostMapping(value = Url.tour.tourCommentsAdd)
+    public ResultDto tourCommentsAdd( @RequestBody Map<String, String> praamMap ){
+        return toutService.tourCommentsAdd ( praamMap );
     }
 
-    /**
-     * 上传文件到七牛云存储
-     * @param multipartFile
-     * @return ResultDto
-     */
-    @PostMapping(value=Url.tour.imgUpload)
-    public ResultDto uploadImgQiniu(@RequestParam("editormd-image-file") MultipartFile multipartFile) throws IOException{
-        FileInputStream inputStream = (FileInputStream) multipartFile.getInputStream();
-        String picName=DateApi.getPicTimeId( );
-        String path = qiniuUtil.uploadImg(inputStream, picName);
-        ResultDto resultDto=new ResultDto(  );
-        resultDto.setCode( SysExcCode.SysCommonExcCode.SYS_SUCCESS );
-        resultDto.setMsg( "上传成功" );
-        resultDto.setData( path );
-        return resultDto;
-    }
 
 }
